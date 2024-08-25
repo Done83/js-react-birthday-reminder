699a01b46792bd5ef8357dbd6f8b0a0c8617a417
Done83 <nedomehan@gmail.com>
2024-08-25 17:04:16 +0200

    Clear and Reset list funcionality button completed

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,27 @@ import Person from "./Person";
 
 const App = () => {
   const [people, setPeople] = useState(data);
+
+  const clearList = () => {
+    setPeople([]);
+  };
+
+  const resetList = () => {
+    setPeople(data);
+  };
   return (
     <div>
       <h1>{people.length} Birthdays today</h1>
       <Person people={people} />
+      {people.length === 0 ? (
+        <button className="btn" onClick={resetList}>
+          Reset List
+        </button>
+      ) : (
+        <button className="btn" onClick={clearList}>
+          Clear List
+        </button>
+      )}
     </div>
   );
 };
